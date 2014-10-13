67cce40ebd002efdeb9a3616cca865ba954cfcd5
Nathan Darmawan <nathandarmawan@gmail.com>
2014-10-13 10:54:39 +0800

    q11

---
+++ q11.R
@@ -0,0 +1,11 @@
+## Q 11
+## In the dataset provided for this Quiz,
+## what are the column names of the dataset?
+
+## Reading Data
+setwd("D:/GitHub/rprog_quiz_week1")
+data <- read.csv("hw1_data.csv")
+
+## Show column names for the dataset
+## Use colnames()
+colnames(data)
